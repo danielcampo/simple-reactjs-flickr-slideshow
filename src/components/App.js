1db1f763f5e6d4aeb2753c37231d6e47665c11f0
Daniel Campo <accounts@danielcampo.com>
2017-11-08 18:32:55 -0500

    Added general styles for app and created initial wireframe design

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 
+import '../styles/App.scss';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <h1>Univision Flickr Gallery</h1>
+      <div id="App">
+        <header id="Header">
+          <div className="header__search">
+            <input type="text" placeholder="Search..." />
+          </div>
         </header>
+        { /* Image Spotlight */ }
+        <div id="Spotlight">
+            <a className="nav nav-prev" href="#">&#9668;</a>
+            <div className="spotlight__image">
+              <img src="http://via.placeholder.com/1500x1000" />
+            </div>
+            <a className="nav nav-next" href="#">&#9658;</a>
+        </div>
+        { /* Image Spotlight */ }
+        <div id="Navigation">
+
+            <div className="image">
+              <a href="#"><img src="http://via.placeholder.com/100x100" /></a>
+            </div>
+            <div className="image">
+              <a href="#"><img src="http://via.placeholder.com/100x100" /></a>
+            </div>
+            <div className="image">
+              <a href="#"><img src="http://via.placeholder.com/100x100" /></a>
+            </div>
+            <div className="image">
+              <a href="#"><img src="http://via.placeholder.com/100x100" /></a>
+            </div>
+            <div className="image">
+              <a href="#"><img src="http://via.placeholder.com/100x100" /></a>
+            </div>
+            <div className="image">
+              <a href="#"><img src="http://via.placeholder.com/100x100" /></a>
+            </div>
+            <div className="image">
+              <a href="#"><img src="http://via.placeholder.com/100x100" /></a>
+            </div>
+          </div>
+
       </div>
     );
   }
