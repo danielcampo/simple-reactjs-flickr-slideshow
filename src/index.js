b8898acb82dfd894448f31635bd8312a3cd8f748
Daniel Campo <accounts@danielcampo.com>
2017-11-08 18:31:50 -0500

    Added import for normalize.css and index styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'normalize.css/normalize.css'
+import './styles/index.scss';
+
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app-main'));
